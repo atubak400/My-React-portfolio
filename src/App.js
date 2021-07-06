47e09b804f3ffc2f5964029404ef3d493f0b4f5e
Kingsley Atuba <atubak40@gmail.com>
2021-07-06 19:25:33 +0100

    Create Containers with colored backgroung

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Topbar from './components/topbar/Topbar'
+import Intro from './components/intro/Intro'
+import Portfolio from './components/portfolio/Portfolio'
+import Works from './components/works/Works'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import './app.scss'
+
 function App() {
 	return (
 		<div className='app'>
-			<h1>React Portfolio</h1>
+			<Topbar />
+			<div className='sections'></div>
+			<Intro />
+			<Portfolio />
+			<Works />
+			<Testimonials />
+			<Contact />
 		</div>
 	)
 }
