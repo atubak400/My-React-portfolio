03421bf6088a8a7731c8e180361b246e7aff299b
Kingsley Atuba <atubak40@gmail.com>
2021-07-16 15:38:31 +0100

    Styled Atuba. logo

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,18 @@
 import './topbar.scss'
 
 export default function topbar() {
-  return <div className='topbar'></div>
+  return (
+    <div className='topbar'>
+      <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro' className='logo'>
+            Atuba.
+          </a>
+        </div>
+        <div className='right'>
+          <h3>menu</h3>
+        </div>
+      </div>
+    </div>
+  )
 }
