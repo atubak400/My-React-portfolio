9a7e9773350f349398a09d6a26aa6932a7388618
Kingsley Atuba <atubak40@gmail.com>
2021-07-19 12:21:24 +0100

    topbar completed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,29 @@
 import './topbar.scss'
+import { Person, Mail } from '@material-ui/icons'
 
-export default function topbar() {
+export default function topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={'topbar ' + (menuOpen && 'active')}>
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>
             Atuba.
           </a>
+          <div className='itemContainer'>
+            <Person className='icon' />
+            <span>+2348189174570</span>
+          </div>
+          <div className='itemContainer'>
+            <Mail className='icon' />
+            <span>atubak40@gmail.com</span>
+          </div>
         </div>
         <div className='right'>
-          <h3>menu</h3>
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
         </div>
       </div>
     </div>
