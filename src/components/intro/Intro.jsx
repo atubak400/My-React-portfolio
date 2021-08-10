e805c65366902c8e8cb93306950733d2859f7fdd
Kingsley Atuba <atubak40@gmail.com>
2021-08-10 16:40:53 +0100

    intro section, left side. profile picture done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,14 @@
 import './intro.scss'
 
 export default function intro() {
-  return <div className='intro' id='intro'></div>
+  return (
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src='assets/mee.jpeg' alt='profile pic' />
+        </div>
+      </div>
+      <div className='right'></div>
+    </div>
+  )
 }
