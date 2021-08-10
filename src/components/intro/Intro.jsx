0b0024a7ceee6f8849e6a9507bf08b7b9508f4ab
Kingsley Atuba <atubak40@gmail.com>
2021-08-10 17:17:22 +0100

    intro section, right side.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,16 @@ export default function intro() {
           <img src='assets/mee.jpeg' alt='profile pic' />
         </div>
       </div>
-      <div className='right'></div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi There, I'm</h2>
+          <h1>Kingsley Atuba</h1>
+          <h3>AWS Cloud Architect / React Engineer</h3>
+        </div>
+        <a href='#portfolio'>
+          <img src='assets/down.png' alt='' />
+        </a>
+      </div>
     </div>
   )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function topbar({ menuOpen, setMenuOpen }) {
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>
-            Atuba..
+            atuba..
           </a>
           <div className='itemContainer'>
             <Person className='icon' />
