eea081bdc56a7bbca6a3518c531143f2f0ba2e3e
Kingsley Atuba <atubak40@gmail.com>
2021-08-11 17:26:55 +0100

    Right intro section text typing animation using ityped and useeffect done!!!

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,18 @@
 import './intro.scss'
 import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
-export default function intro() {
+export default function Intro() {
+  const textRef = useRef()
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Developer', 'Designer', 'Content Creator'],
+    })
+  }, [])
   return (
     <div className='intro' id='intro'>
       <div className='left'>
@@ -14,7 +25,7 @@ export default function intro() {
           <h2>Hi There, I'm</h2>
           <h1>Kingsley Atuba</h1>
           <h3>
-            AWS Cloud Architect <span>React Developer</span>
+            AWS Cloud Architect <span ref={textRef}></span>
           </h3>
         </div>
         <a href='#portfolio'>
