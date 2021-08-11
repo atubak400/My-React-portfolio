928b9bf4c6095ee74b260213fb1ff8fa4f03aa86
Kingsley Atuba <atubak40@gmail.com>
2021-08-11 18:02:36 +0100

    building portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,25 @@
 import './portfolio.scss'
 
 export default function portfolio() {
-  return <div class='portfolio' id='portfolio'></div>
+  return (
+    <div class='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>React</li>
+        <li>HTML and CSS</li>
+        <li>Wordpress</li>
+        <li>Bootstrap</li>
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img
+            src='https://appcanvas.io/wp-content/uploads/2019/10/john.jpg'
+            alt=''
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  )
 }
